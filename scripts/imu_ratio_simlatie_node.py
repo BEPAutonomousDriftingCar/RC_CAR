9271ef3c48e72763dc3f7e74371a659993875840
Vongeo <georgevonhoff@gmail.com>
2018-05-03 13:37:11 +0200

    Update imu_ratio_simlatie_node.py

--- scripts/imu_ratio_simlatie_node.py
+++ scripts/imu_ratio_simlatie_node.py
@@ -9,7 +9,7 @@ msg = """
 Im running
 """
 def imu_ratio():          
-    pub = rospy.Publisher('fake_imu_ratio', Twist, queue_size=10)   
+    pub = rospy.Publisher('fake_imu_ratio', String, queue_size=10)   
     rospy.init_node('imu_ratio_node', anonymous=True)      
     rate = rospy.Rate(10) # 10hz                       
     while not rospy.is_shutdown():                      
@@ -18,18 +18,12 @@ def imu_ratio():
           rate = rospy.Rate(10)
           while ratio > 0.9:
             		ratio = ratio - 0.01
-            		twist = Twist()
-	   		twist.linear.x = ratio; twist.linear.y = 0; twist.linear.z = 0;
-			twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0;
-			pub.publish(twist)                            
+			pub.publish(ratio)                            
             		rate.sleep() 
           rate = rospy.Rate(10)
           while ratio < 1.1:
             		ratio = ratio + 0.01
-            		twist = Twist()
-			twist.linear.x = ratio; twist.linear.y = 0; twist.linear.z = 0;
-			twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0;
-			pub.publish(twist)                            
+			pub.publish(ratio)                            
             		rate.sleep() 
         
 if __name__ == '__main__':
