41db8bc9e63398eac33c2bf1e35d894f40f1ff4b
Vongeo <georgevonhoff@gmail.com>
2018-05-03 12:47:47 +0200

    Update listener_test.py

--- scripts/listener_test.py
+++ scripts/listener_test.py
@@ -15,7 +15,7 @@ def listener():
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("chatter", String, callback)
+    rospy.Subscriber("fake_imu_ratio", Twist, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
