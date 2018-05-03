60ef31997e06b567b1551b11dbea9c4c42c700ca
Vongeo <georgevonhoff@gmail.com>
2018-05-03 11:17:55 +0200

    Update imu_ratio_simlatie_node.py

--- scripts/imu_ratio_simlatie_node.py
+++ scripts/imu_ratio_simlatie_node.py
@@ -15,20 +15,20 @@ def imu_ratio():
         for i in range(4):
           rate = rospy.Rate(10)
           while ratio > 0.9:
-            ratio = ratio - 0.01
-            twist = Twist()
-			      twist.linear.x = ratio; twist.linear.y = 0; twist.linear.z = 0;
+            		ratio = ratio - 0.01
+            		twist = Twist()
+	   				twist.linear.x = ratio; twist.linear.y = 0; twist.linear.z = 0;
 				    twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0;
 				    pub.publish(twist)                            
-            rate.sleep() 
+            		rate.sleep() 
           rate = rospy.Rate(10)
           while ratio < 1.1:
-            ratio = ratio + 0.01
-            twist = Twist()
-			      twist.linear.x = ratio; twist.linear.y = 0; twist.linear.z = 0;
+            		ratio = ratio + 0.01
+            		twist = Twist()
+			     	twist.linear.x = ratio; twist.linear.y = 0; twist.linear.z = 0;
 				    twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0;
 				    pub.publish(twist)                            
-            rate.sleep() 
+            		rate.sleep() 
         
 if __name__ == '__main__':
     try:                                            
