3e8d59fdb812c3b4eb2d9c081b7510b0be1e3cbd
Vongeo <georgevonhoff@gmail.com>
2018-05-03 11:10:55 +0200

    Update and rename imu_simlatie_node.py to imu_ratio_simlatie_node.py

---
+++ scripts/imu_ratio_simlatie_node.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python               # zorgt ervoor dat de scripts word geexecute als een python script
+# license removed for brevity
+import rospy          #importeer de rospy library
+import math
+from std_msgs.msg import String #importeer de string message om te gebruiken voor publishing
+from geometry_msgs.msg import Twist
+
+
+def imu_ratio():          
+    pub = rospy.Publisher('fake_imu_ratio', Twist, queue_size=10)   
+    rospy.init_node('imu_ratio_node', anonymous=True)      
+    rate = rospy.Rate(10) # 10hz                       
+    while not rospy.is_shutdown():                      
+        ratio = 1
+        for i in range(4):
+          rate = rospy.Rate(10)
+          while ratio > 0.9:
+            ratio = ratio - 0.01
+            twist = Twist()
+			      twist.linear.x = ratio; twist.linear.y = 0; twist.linear.z = 0;
+				    twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0;
+				    pub.publish(twist)                            
+            rate.sleep() 
+          rate = rospy.Rate(10)
+          while ratio < 1.1:
+            ratio = ratio + 0.01
+            twist = Twist()
+			      twist.linear.x = ratio; twist.linear.y = 0; twist.linear.z = 0;
+				    twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0;
+				    pub.publish(twist)                            
+            rate.sleep() 
+        
+if __name__ == '__main__':
+    try:                                            
+        imu_ratio()
+    except rospy.ROSInterruptException:
+        pass
