28ad7d08c3d8a3f18b133dc45660a8e6d98f1768
Vongeo <georgevonhoff@gmail.com>
2018-05-21 22:33:02 +0200

    Update deadmancombo2.py

--- scripts/deadmancombo2.py
+++ scripts/deadmancombo2.py
@@ -5,8 +5,8 @@ import math
 
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
+from std_msgs.msg import Bool
 
-import sys, select, termios, tty
 msg = """
 Reading from the keyboard  and Publishing to Twist!
 """
@@ -14,38 +14,30 @@ Reading from the keyboard  and Publishing to Twist!
 def talker(data):
    	global twist
     	twist = Twist()
-	twist.linear.x = data.linear.x
-    	twist.angular.z = data.angular.z
+	twist.linear.x = data.linear.x*bool
+    	twist.angular.z = data.angular.z*bool
     	pub.publish(twist)
 	
-def getKey():
-	tty.setraw(sys.stdin.fileno())
-	select.select([sys.stdin], [], [], 0)
-	key = sys.stdin.read(1)
-	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-	return key
 
 def safety():
-	global twist
-	twist = Twist()
-    	twist.linear.x = 0
-    	twist.angular.z = 0
+	global bool
+	bool = Bool()
+    	bool = data
     	# If the motor has reached its limit, publish a new command.
-    	pub.publish(twist)
 
 def listener():
-    global twist
-    rospy.Subscriber('/cmd_vel_sim', Twist, talker)
-    # Initial movement.
-    twist = Twist()
-    twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0;
-    twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0;
-    pub.publish(twist)
-    rospy.spin()
+    	global twist
+   	rospy.Subscriber('/cmd_vel_sim', Twist, talker)
+	rospy.Subscriber('/cmd_vel_safety', Twist, safety)
+   	 # Initial movement.
+    	twist = Twist()
+    	twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0;
+   	twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0;
+   	pub.publish(twist)
+    	rospy.spin()
 
 
 if __name__ == '__main__':
-    	settings = termios.tcgetattr(sys.stdin)
 	pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
 	rospy.init_node('deadman', anonymous=True)
 	try:
