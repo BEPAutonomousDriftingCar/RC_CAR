01d3488f9bfeaeb8d63225e5b3d162ae99ddf454
Vongeo <georgevonhoff@gmail.com>
2018-05-22 08:53:47 +0200

    Update dm.py

--- scripts/dm.py
+++ scripts/dm.py
@@ -12,32 +12,32 @@ Reading from the keyboard  and Publishing to Twist!
 """
 
 def listener1():
-    global twist
-    rospy.Subscriber('/dm_init', Twist, talker2)
-	rospy.Subscriber('/cmd_vel_sim', Twist, talker1)
-    # Initial movement.
-    #twist = Twist()
-    #twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0;
-    #twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0;
-    #pub.publish(twist)
-    rospy.spin()
+    	global twist
+   	rospy.Subscriber('/dm_init', Twist, talker2)
+    	rospy.Subscriber('/cmd_vel_sim', Twist, talker1)
+    	# Initial movement.
+    	#twist = Twist()
+    	#twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0;
+    	#twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0;
+    	#pub.publish(twist)
+    	rospy.spin()
 	
 def talker1(data):
-    global dm
+    	global dm
 	global twist
 	x=data.linear.x
-    z=data.angular.z
-    twist = Twist()
-    #twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0;
-    #twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0;
-    twist.linear.x = data.linear.x * dm 
-    twist.angular.z = data.angular.z * dm
-    pub.publish(twist)
+    	z=data.angular.z
+    	twist = Twist()
+    	#twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0;
+    	#twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0;
+    	twist.linear.x = data.linear.x * dm 
+    	twist.angular.z = data.angular.z * dm
+    	pub.publish(twist)
 	
 def talker2(data):
-    global dm
+    	global dm
 	dm = data.linear.x
-    return dm
+    	return dm
 	
 if __name__ == '__main__':
 	pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
