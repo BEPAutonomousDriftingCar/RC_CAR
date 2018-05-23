e5131b52e3eb04fdafd9caca94735115bb8cbdff
AnandSie <38072667+AnandSie@users.noreply.github.com>
2018-05-23 10:30:15 +0200

    Update print_info.py

--- scripts/print_info.py
+++ scripts/print_info.py
@@ -1,7 +1,17 @@
 #! /usr/bin/evn python
 
-import rospy
+#import rospy
 
-rospy.init_node('print_info')
-print 'INFORMATION: All essential nodes are now running. You can connect simulink to the car via two ways: /cmd_vel or /cmd_vel_sim (use second if you want emercency button). Start rc_car dm_init.py for emercency button'
-rospy.spin()
+#rospy.init_node('print_info')
+#print 'INFORMATION: All essential nodes are now running. You can connect simulink to the car via two ways: /cmd_vel or /cmd_vel_sim (use second if you want emercency button). Start rc_car dm_init.py for emercency button'
+#rospy.spin()
+
+
+
+def main():
+    #rospy.init_node('print_to_screen', anonymous=True)
+    print("Please keep this running in a separate tab.")
+    #rospy.spin()
+
+if __name__ == '__main__':
+    main()
