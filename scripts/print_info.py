632048cce877ded645e57a0b1319962d6298f764
AnandSie <38072667+AnandSie@users.noreply.github.com>
2018-05-23 11:37:18 +0200

    Update print_info.py

--- scripts/print_info.py
+++ scripts/print_info.py
@@ -10,8 +10,7 @@ import rospy
 
 def main():
     rospy.init_node('print_info', anonymous=True)
-    print("INFORMATION: All essential nodes are now running. You can connect simulink to the car via two ways: /cmd_vel or /cmd_vel_sim (use second if you want emercency button). Start rc_car dm_init.py for emercency button
-")
+    print("INFORMATION: All essential nodes are now running. You can connect simulink to the car via two ways: /cmd_vel or /cmd_vel_sim (use second if you want emercency button). Start rc_car dm_init.py for emercency button")
     rospy.spin()
 
 if __name__ == '__main__':
